bbb714e4cd06da17133290f25dd5591a2abc3305
Zacharias Hellberg <zacharias.hellberg@student.oulu.fi>
2017-01-26 21:32:44 +0200

    Removed unnecessary files

--- resrec.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import argparse
-import cv2
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help="Give path to image as argument.")
-args = vars(ap.parse_args())
-
-img = cv2.imread(args["image"])
