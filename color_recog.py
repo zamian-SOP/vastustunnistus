04f95b26d21f9bff90154fd3b8f8c65aac7940dd
Zacharias Hellberg <zacharias.hellberg@student.oulu.fi>
2017-01-26 19:57:03 +0200

    Working and commented version

--- color_recog.py
+++ color_recog.py
@@ -1,44 +1,42 @@
-# import the necessary packages
+#Import necessary packages
 import numpy as np
 import argparse
 import cv2
 
-# construct the argument parse and parse the arguments
+#Create argument parser
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", help = "path to the image")
 args = vars(ap.parse_args())
 
-# load the image
+#Read image
 image = cv2.imread(args["image"])
 
-# define the list of boundaries
+#Create list of boundaries
 boundaries = [
-	([17, 15, 100], [50, 56, 200]),
-	([86, 31, 4], [220, 88, 50]),
-	([25, 146, 190], [62, 174, 250]),
-	([103, 86, 65], [145, 133, 128])
+	([90, 80, 60], [150, 140, 130]),
+	([80, 25, 0], [220, 95, 60]),
+	([25, 140, 180], [75, 180, 250]),
+	([10, 10, 100], [60, 60, 200])
 ]
 
+#Create a black version of given image as background
 black = [0,0,0]
 blacknp = np.array(black, dtype = "uint8")
 blackmask = cv2.inRange(image, blacknp, blacknp)
-filtered = cv2.bitwise_and(image,image,mask=blackmmask)
+filtered = cv2.bitwise_and(image,image,mask=blackmask)
 
-for x in boundaries:
-# loop over the boundaries
-	for (lower, upper) in boundaries:
-		# create NumPy arrays from the boundaries
-		lower = np.array(lower, dtype = "uint8")
-		upper = np.array(upper, dtype = "uint8")
+#Loop through boundaries
+for (lower, upper) in boundaries:
+	#Make numpy arrays out of boundaries
+	lower = np.array(lower, dtype = "uint8")
+	upper = np.array(upper, dtype = "uint8")
+	#Create mast using lower and upper boundaries
+	mask = cv2.inRange(image, lower, upper)
+	#Apply mask to image
+	output = cv2.bitwise_and(image, image, mask = mask)
+	#Add the filtered color to final image
+	filtered = cv2.addWeighted(filtered,1, output,1, 0)
 
-		# find the colors within the specified boundaries and apply
-		# the mask
-		mask = cv2.inRange(image, lower, upper)
-		output = cv2.bitwise_and(image, image, mask = mask)
-		# show the images
-		#cv2.imshow("images", np.hstack([image, output]))
-		#cv2.waitKey(0)
-
-	filtered = cv2.addWeighted(filtered,0.5, output,0.5, 0)
-	cv2.imshow("images", np.hstack([output,filtered]))
-	cv2.waitKey(0)
+#Show result and wait for user to close window
+cv2.imshow("images", np.hstack([filtered,image]))
+cv2.waitKey(0)
